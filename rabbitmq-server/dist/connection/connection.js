80d4d05eb1b1c1e64e8000b24b683f36f712b8d3
omnyx2 <omnyx2@gmail.com>
2022-01-15 16:55:22 +0900

    last

--- rabbitmq-server/dist/connection/connection.js
+++ rabbitmq-server/dist/connection/connection.js
@@ -5,9 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 //amqp데이터 타입을 설치해야함
 const callback_api_1 = __importDefault(require("amqplib/callback_api"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 //amqp 기본 포트 5672, 관리자 사이트 포트-15672
-// const amqpURL: string = 'amqp://guest:guest@127.0.0.1:5672'
-const amqpURL = 'amqps://avrkjsxl:W-rDWNQ7k-xB-uvgOui-M5X1Dx9NxsfO@dingo.rmq.cloudamqp.com/avrkjsxl';
+const amqpURL = process.env.RABBITMQ_URL || 'amqp://guest:guest@127.0.0.1:5672';
 exports.default = (callback) => {
     callback_api_1.default.connect(amqpURL, (error, conection) => {
         if (error) {
--- rabbitmq-server/src/connection/connection.ts
+++ rabbitmq-server/src/connection/connection.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 //amqp 기본 포트 5672, 관리자 사이트 포트-15672
 const amqpURL: string = process.env.RABBITMQ_URL ||'amqp://guest:guest@127.0.0.1:5672'
-
+  
 export default (callback: Function) => {
   amqp.connect(amqpURL,
     (error: any, conection: Connection) => {
