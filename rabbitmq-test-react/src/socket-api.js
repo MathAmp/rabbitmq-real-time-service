416f7525d744a831f23287087636e0639aeea81d
mathamp <jwzz123123@gmail.com>
2022-01-19 23:35:03 +0900

    Change Address

--- rabbitmq-test-react/src/socket-api.js
+++ rabbitmq-test-react/src/socket-api.js
@@ -1,6 +1,6 @@
 import clientSocket from 'socket.io-client';
 
-export const API_URL = "http://localhost:5555";
+export const API_URL = "http://batatas.iptime.org:5555";
 const socket = clientSocket(`${API_URL}/calc`);
 
 export const subscribe = (newCallback) => {
